c3fcb93ae47b0d754a1ade1fcd439c5d1c6bca24
Hxohu <yang220400354@gmail.com>
2025-03-19 19:59:05 +0800

    add centered_binomial_distribution, convolution, product function

---
+++ README.md
@@ -0,0 +1,3 @@
+# LWE based schemes decryption failures analysis
+In this project, we want to make a software to compute the decryption failure probability of LWE based schemes and it's variants automatically to
+make it easier when design a new scheme.
--- prob_util.py
+++ prob_util.py
@@ -1,15 +1,51 @@
+from math import comb
+
 def uniform_distribution(eta):
     """
     Probability density function of the uniform distribution for -eta to eta 
     :param eta: integer
     :returns: A dictionnary {x:p(x) for x in {-eta .. eta}}
     """
+
     D = {}
-    
     for i in range(-eta, eta + 1):
         D[i] = 1 / (2*eta + 1)
 
     return D
 
-def binomial_distribution():
-    pass
+def centered_binomial_distribution(eta):
+    """
+    Probability density function of the centered binomial distribution for -eta to eta
+    :param eta: integer
+    :returns: A dictionary {x:p(x) for x in {-eta .. eta}}
+    """
+    
+    return {k: comb(2*eta, eta +k) * (0.5)**(2*eta) for k in range(-eta, eta+1)}
+
+def convolution_law(A, B):
+    """
+    Construct the convolution of two laws (sum of independent variables from two input laws)
+    :param A: first input law (dictionary)
+    :param B: second input law (dictionary)
+    """
+
+    C = {}
+    for a in A:
+        for b in B:
+            c = a + b
+            C[c] = C.get(c, 0) + A[a] * B[b]
+    return C
+
+
+def product_law(A, B):
+    """ Construct the law of the product of independent variables from two input laws
+    :param A: first input law (dictionary)
+    :param B: second input law (dictionary)
+    """
+
+    C = {}
+    for a in A:
+        for b in B:
+            c = a*b
+            C[c] = C.get(c, 0) + A[a] * B[b]
+    return C
